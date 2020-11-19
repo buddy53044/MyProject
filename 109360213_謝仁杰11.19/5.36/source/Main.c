191aca648594e46186eff5787b88c336c78b73e5
buddy53044 <buddy53044@gmail.com>
2020-11-19 19:56:31 +0800

    report

--- 109360213_謝仁杰11.19/5.36/source/Main.c
+++ 109360213_謝仁杰11.19/5.36/source/Main.c
@@ -3,25 +3,21 @@
 
 void hanoi(int n, char A, char B, char C);
 
-int main()
-{
+int main() {
 	int n;
 	printf("½Ð¿é¤J½L¼Æ¡G");
-	scanf_s("%d", &n);
+//	scanf_s("%d", &n);
+ 	n=2;
 	hanoi(n, 'A', 'B', 'C');
 
 	system("pause");
-	return 0;
+   	return 0;
 }
 
-void hanoi(int n, char A, char B, char C)
-{
-	if (n == 1) 
-	{
+void hanoi(int n, char A, char B, char C) {
+	if (n == 1) {
 		printf("Move sheet from %c to %c\n", A, C);
-	}
-	else
-	{
+	} else {
 		hanoi(n - 1, A, C, B);
 		hanoi(1, A, B, C);
 		hanoi(n - 1, B, A, C);
