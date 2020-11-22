4122e59a7a9a655723d7daedd9b585819032ea53
buddy53044 <buddy53044@gmail.com>
2020-11-22 14:53:12 +0800

    hw11.22

--- 109360213_謝仁杰11.22/6.11/source/Main.c
+++ 109360213_謝仁杰11.22/6.11/source/Main.c
@@ -1,11 +1,47 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
+#define size 14
+int main() {
 
-int main()
-{
+
+	clock_t begin, end;
+	double cost;
+	//開始記錄
+	begin = clock();
+	
+	/*待測試程式段*/
 
 
 
-system("pause");
-return 0;
+	int n[size] = { 9,2,3,1,5,4,8,7,10,6,12,16,87,95 };
+	int flag=1;
+	int temp;
+	int i,j;
+	for(i=0; i<size; i++) {
+		flag=1;
+		for(j=0; j<size-i; j++) {
+//		for(j=0;j<size;j++){
+			if(n[j]>n[j+1]) {
+				temp=n[j];
+				n[j]=n[j+1];
+				n[j+1]=temp;
+				flag=0;
+			}
+		}
+		if(flag==1)
+			break;
+	}
+
+	for(i=0; i<size; i++) {
+		printf("%d ",n[i]);
+	}
+
+	//結束記錄
+	end = clock();
+	cost = (double)(end - begin)/CLOCKS_PER_SEC;
+	printf("constant CLOCKS_PER_SEC is: %ld, time cost is: %lf secs", CLOCKS_PER_SEC, cost);
+
+//system("pause");
+	return 0;
 }
